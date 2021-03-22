39c48a19a2e875cebf28c3121dc2b55c3f167bde
nomansnick <marapatgab@gmail.com>
2021-03-22 13:53:31 +0100

    icon and formatting to title

--- frontend/realestate/src/BrowseAll.js
+++ frontend/realestate/src/BrowseAll.js
@@ -1,13 +1,17 @@
 import React from "react";
+import "./style.css";
 
 export default function BrowseAll(props) {
   const { title } = props;
   return (
-    <div>
-      <div>
-        <h1 className="font-weight-bold">{title}</h1>
+    <div className="p-3 d-flex flex-column align-items-center">
+      <div className="col-xs-12 col-md-6">
+        <div className="d-flex flex-row justify-content-between">
+          <h1 className="font-weight-bold">{title}</h1>
+          <i className="fa fa-map-marker fa-2x orange" aria-hidden="true"></i>
+        </div>
+        <div>2</div>
       </div>
-      <div>2</div>
     </div>
   );
 }

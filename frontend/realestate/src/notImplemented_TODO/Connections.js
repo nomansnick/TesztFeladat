c90222f5cea21ca6fea6c365aae52d3a6bd4bfa8
Mara Patrik <marapatgab@gmail.com>
2021-03-25 12:36:59 +0100

    return in connections

--- frontend/realestate/src/notImplemented_TODO/Connections.js
+++ frontend/realestate/src/notImplemented_TODO/Connections.js
@@ -3,6 +3,7 @@ import SearchWithIcon from "../SearchWithIcon";
 import "../../node_modules/font-awesome/css/font-awesome.min.css";
 import useContacts from "../hook/useContacts";
 import { Image } from "react-bootstrap";
+import LinkBack from "../LinkBack";
 
 const thumbnailFix = {
   maxWidth: "8rem",
@@ -15,19 +16,22 @@ export default function Connections() {
     <>
       {items ? (
         <div className="d-flex justify-content-center h-100 w-100">
-          <div className="d-flex flex-column align-items-center p-4 col-xs-12 col-md-6 bg-primary rounded1Rem">
-            <div className="mb-5">
+          <div className="d-flex flex-column justify-content-around align-items-center w-100 h-100 p-5 bg-primary rounded1Rem">
+            <div>
               <h3>Connections</h3>
               <div>NOT IMPLEMENTED!!!</div>
+              <div>
+                <LinkBack />
+              </div>
             </div>
-            <div className="mb-5 w-100">
+            <div className="w-100">
               <SearchWithIcon
                 placeHolder="&#xF002; Who are you looking for?"
                 label="people"
               />
             </div>
             <div className="d-flex flex-column align-items-start w-100">
-              <div className="mb-5 w-100">Dummy Contacts:</div>
+              <div className="w-100">Dummy Contacts:</div>
               <div className="w-100">
                 {items.map((onePerson) => (
                   <div className="d-flex align-items-center justify-content-around w-100 ml-n4">

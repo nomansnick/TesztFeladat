af42583a2c2a1451ad111fe616926c2092371783
Mara Patrik <marapatgab@gmail.com>
2021-03-25 01:00:46 +0100

    buttons resize the rest

--- frontend/realestate/src/App.js
+++ frontend/realestate/src/App.js
@@ -31,32 +31,47 @@ function App() {
   const [deviceCycle, setDeviceCycle] = useState(0);
   const [device, setDevice] = useState(devices[deviceCycle]);
   const [typoCycle, setTypoCycle] = useState(0);
-  const [typography, setTypography] = useState(typographies[typoCycle]);
+  const [typography, setTypography] = useState(typographies[typoCycle].type);
   const [resCycle, setResCycle] = useState(0);
   const [resolution, setResolution] = useState(
-    resolutions.SmartPhone[resCycle]
+    resolutions.SmartPhone[resCycle].size
   );
+  const [classToUse, setClassToUse] = useState("smartPhoneGS5 tNR");
+  const [usedSize, setUsedSize] = useState(0);
 
   function nextDevice(param) {
     if (param >= devices.length - 1) {
       setDevice(devices[0]);
       setDeviceCycle(0);
-      setResolution(resolutions[devices[0]][0]);
+      setResolution(resolutions[devices[0]][0].size);
+      setUsedSize(0);
+      classNameBuilder(resolutions[devices[0]][0].className, typography);
       return;
     }
     setDevice(devices[param + 1]);
     setDeviceCycle(param + 1);
-    setResolution(resolutions[devices[param + 1]][0]);
+    setResolution(resolutions[devices[param + 1]][0].size);
+    setUsedSize(0);
+    classNameBuilder(resolutions[devices[param + 1]][0].className, typography);
   }
 
   function nextTypo(param) {
     if (param >= typographies.length - 1) {
-      setTypography(typographies[0]);
+      setTypography(typographies[0].type);
       setTypoCycle(0);
+      console.log(usedSize);
+      classNameBuilder(
+        resolutions[device][usedSize].className,
+        typographies[0].className
+      );
       return;
     }
-    setTypography(typographies[param + 1]);
+    setTypography(typographies[param + 1].type);
     setTypoCycle(param + 1);
+    classNameBuilder(
+      resolutions[device][usedSize].className,
+      typographies[param + 1].className
+    );
   }
 
   function nextRes(param) {
@@ -66,20 +81,27 @@ function App() {
     if (device === devices[1]) {
       nextResInner(param, resolutions.Tablet);
     }
-    if (device === devices[3]) {
+    if (device === devices[2]) {
       nextResInner(param, resolutions.Pc);
     }
   }
 
   function nextResInner(param, array) {
     if (param >= array.length - 1) {
-      setResolution(array[0]);
+      setResolution(array[0].size);
       setResCycle(0);
+      classNameBuilder(array[0].className, typography);
       return;
     }
-    setResolution(array[param]);
+    setResolution(array[param + 1].size);
     setResCycle(param + 1);
+    classNameBuilder(array[param + 1].className, typography);
     return;
+  }
+
+  function classNameBuilder(size, font) {
+    let classBase = "";
+    setClassToUse(classBase.concat(" ").concat(size).concat(" ").concat(font));
   }
 
   return (
@@ -98,7 +120,7 @@ function App() {
             resCycle={resCycle}
           />
         </div>
-        <div className="w-100 h-100">
+        <div className={classToUse}>
           <Routes />
         </div>
       </div>
--- frontend/realestate/src/settings/Settings.js
+++ frontend/realestate/src/settings/Settings.js
@@ -1,11 +1,26 @@
 const devices = ["SmartPhone", "Tablet", "Pc"];
 
-const typographies = ["Times New Roman", "Arial", "Verdana", "Free Font"];
+const typographies = [
+  { type: "Times New Roman", className: "tNR" },
+  { type: "Arial", className: "arial" },
+  { type: "Verdana", className: "verdana" },
+];
 
 const resolutions = {
-  SmartPhone: ["360 x 460", "375 x 812", "411 x 731"],
-  Tablet: ["768 x 1024", "1024 x 1366"],
-  Pc: ["1920 x 1080", "1600 x 900", "1024 x 768"],
+  SmartPhone: [
+    { size: "360 x 460", className: "smartPhoneGS5" },
+    { size: "411 x 731", className: "smartPhonePixel" },
+    { size: "375 x 812", className: "smartPhoneI10" },
+  ],
+  Tablet: [
+    { size: "768 x 1024", className: "tablet" },
+    { size: "1024 x 1366", className: "tabletPro" },
+  ],
+  Pc: [
+    { size: "1024 x 768", className: "pcScreen1024x768" },
+    { size: "1600 x 900", className: "pcScreen1600x900" },
+    { size: "1920 x 1080", className: "pcScreen1920x1080" },
+  ],
 };
 
 export { devices, typographies, resolutions };
